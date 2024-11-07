00abd6b81af37b9e71781bd713aa1ea98deaa404
David810209 <a1050120bc@gmail.com>
2024-11-08 05:20:54 +0800

    1108 add audocast, gradscalar, change to MSE loss

--- eval_result.py
+++ eval_result.py
@@ -6,60 +6,54 @@ from PIL import Image
 from tqdm import tqdm
 from unet_utils import CustomDataset, UNet
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 import cv2
-import random
 import shutil
 
-test_num = 1
+# 設定模型檔案路徑
+model_path = 'model_1107_2.pt'
+
 def load_model(device):
-    path = 'model_1107_3.pt'
-    model = UNet()
-    model = model.to(device)  
-    model.load_state_dict(torch.load(path, map_location=device)['model_state_dict'])
+    # 載入訓練好的 UNet 模型
+    model = UNet().to(device)
+    model.load_state_dict(torch.load(model_path, map_location=device)['model_state_dict'])
     return model
 
 def save_output_as_image(tensor, output_dir, filename):
-    # 将预测结果从 [-1, 1] 转换回 [0, 1]
+    # 將模型輸出保存為圖像，並進行二值化處理
     output_image = tensor.squeeze().cpu().numpy()
-    # output_image = (output_image + 1) / 2  # 先转回[0,1]
-    print(output_image)
-    binary_mask = (output_image > 0.5).astype(np.uint8) * 255  # 二值化并转换为0-255范围
+    binary_mask = (output_image > 0.5).astype(np.uint8) * 255
     image = Image.fromarray(binary_mask)
     image.save(os.path.join(output_dir, filename))
 
-
-
-def evaluate_model(model, test_loader,  output_dir='output_images', device='cuda'):
+def evaluate_model(model, test_loader, output_dir='output_images', device='cuda'):
+    # 評估模型並將結果保存為圖像
     model.eval()
-    total_loss = 0
     num_correct = 0
     num_pixels = 0
-    
+
     # 確保輸出目錄存在
     os.makedirs(output_dir, exist_ok=True)
-    
-    # 使用 tqdm 顯示進度條
+
     with torch.no_grad():
         for idx, (x, y, filename) in enumerate(tqdm(test_loader, desc="Evaluating", unit="batch")):
             x = x.to(device)
             y = y.to(device)
-            
-            # 推論
+
+            # 推論並應用 sigmoid 函數
             output = model(x)
             output = torch.sigmoid(output)
-            print(output)
+
+            # 二值化輸出
             predictions = (output > 0.5).float()
             
-            
-            # 計算準確度 (Accuracy)
+            # 計算準確度
             num_correct += (predictions == y).sum().item()
             num_pixels += torch.numel(predictions)
-            
-            # 儲存每張圖片的推論結果
+
+            # 保存每張圖片的推論結果
             for i in range(predictions.size(0)):
                 save_output_as_image(predictions[i], output_dir, f"out_{filename[i]}")
-    
+
     accuracy = num_correct / num_pixels
     print(f"Accuracy: {accuracy * 100:.2f}%")
     return accuracy
@@ -69,53 +63,49 @@ def sort_nicely(file_list):
     import re
     return sorted(file_list, key=lambda x: [int(i) if i.isdigit() else i for i in re.split(r'(\d+)', x)])
 
-
 def combine_images(input_folder, label_folder, model_output_folder, output_folder):
-    # 获取文件夹中的所有图像文件
+    # 定義目標圖像大小資訊
     images_info = [
-       (723, 668), (667, 654),(583, 678),
-       (710, 797), (600, 722), (840, 703),
-       (710, 683), (773, 645), (738, 820)
+        (723, 668), (667, 654), (583, 678),
+        (710, 797), (600, 722), (840, 703),
+        (710, 683), (773, 645), (738, 820)
     ]
-    input_images = sort_nicely([f for f in os.listdir(input_folder) if f.endswith(('.jpg'))])
-    label_images = sort_nicely([f for f in os.listdir(label_folder) if f.endswith(('.jpg'))])
-    model_output_images = sort_nicely([f for f in os.listdir(model_output_folder) if f.endswith(('.jpg'))])
 
-    # 确保文件夹中的图像数量相同
+    input_images = sort_nicely([f for f in os.listdir(input_folder) if f.endswith('.jpg')])
+    label_images = sort_nicely([f for f in os.listdir(label_folder) if f.endswith('.jpg')])
+    model_output_images = sort_nicely([f for f in os.listdir(model_output_folder) if f.endswith('.jpg')])
     num_images = min(len(input_images), len(label_images), len(model_output_images))
 
     if num_images == 0:
-        print("没有找到任何图像，请检查文件夹是否包含图像。")
+        print("沒有找到任何圖像，請檢查文件夾是否包含圖像。")
         return
-
-    # 创建输出文件夹（如果不存在）
+    
+    test_num = input_image[0].split('_')[0]
     os.makedirs(output_folder, exist_ok=True)
     target_width, target_height = images_info[test_num-1]
+    
     for i in range(num_images):
-        # 构造每张图像的完整路径
         input_image_path = os.path.join(input_folder, input_images[i])
         label_image_path = os.path.join(label_folder, label_images[i])
         model_output_image_path = os.path.join(model_output_folder, model_output_images[i])
 
-        # 读取图片
         input_image = cv2.imread(input_image_path)
         label_image = cv2.imread(label_image_path)
         model_output_image = cv2.imread(model_output_image_path)
 
-        # 检查每张图片是否成功读取
         if input_image is None or label_image is None or model_output_image is None:
-            print(f"无法读取以下图像之一: {input_image_path}, {label_image_path}, {model_output_image_path}")
+            print(f"無法讀取以下圖像之一: {input_image_path}, {label_image_path}, {model_output_image_path}")
             continue
 
-        # 确保图片格式一致，转成 RGB
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
         label_image = cv2.cvtColor(label_image, cv2.COLOR_BGR2RGB)
         model_output_image = cv2.cvtColor(model_output_image, cv2.COLOR_BGR2RGB)
 
-        # 调整图像大小为 560x512
+        # 調整圖像大小
         input_image_resized = cv2.resize(input_image, (target_width, target_height), interpolation=cv2.INTER_LANCZOS4)
         label_image_resized = cv2.resize(label_image, (target_width, target_height), interpolation=cv2.INTER_LANCZOS4)
         model_output_image_resized = cv2.resize(model_output_image, (target_width, target_height), interpolation=cv2.INTER_LANCZOS4)
+
         def pad_image(image, target_size=(840, 820), color=(255, 255, 255)):
             padded_image = np.full((target_size[1], target_size[0], 3), color, dtype=np.uint8)
             height, width = image.shape[:2]
@@ -123,73 +113,36 @@ def combine_images(input_folder, label_folder, model_output_folder, output_folde
             y_offset = (target_size[1] - height) // 2
             padded_image[y_offset:y_offset + height, x_offset:x_offset + width] = image
             return padded_image
+
         input_image_padded = pad_image(input_image_resized)
         label_image_padded = pad_image(label_image_resized)
         model_output_image_padded = pad_image(model_output_image_resized)
-        height, width, _ = input_image_padded.shape  # 获取图像高度和宽度
-        offset = 10  # 设置偏移量
 
-        # 添加文字标记，在每个图像的左下角
-        cv2.putText(input_image_padded, input_images[i], (10, height - offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-        cv2.putText(label_image_padded, label_images[i], (10, height - offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-        cv2.putText(model_output_image_padded, model_output_images[i], (10, height - offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-
-
-        # 合并三张图片
         combined_image = np.hstack((input_image_padded, label_image_padded, model_output_image_padded))
-
-        # 构造输出文件名，使用正确的索引
         output_filename = f"combined_{input_images[i]}"
         output_path = os.path.join(output_folder, output_filename)
-
-        # 保存合并后的图片
         cv2.imwrite(output_path, cv2.cvtColor(combined_image, cv2.COLOR_RGB2BGR))
-        # print(f"Combined image saved to {output_path}")
-
-def split_image():
-    data_folder = 'data'
-    data_mask_folder = 'data_mask'
-    test_folder = 'test'
-    test_mask_folder = 'test_mask'
-    os.makedirs(test_folder, exist_ok=True)
-    os.makedirs(test_mask_folder, exist_ok=True)
-
-    # 获取所有文件夹中所有符合条件的图像
-    image_files = [f for f in os.listdir(data_folder) if f.endswith('.jpg')]
-
-    # 随机选择500张图片
-    selected_images = random.sample(image_files, min(500, len(image_files)))
-
-    for filename in selected_images:
-        # 复制图像到测试文件夹
-        src = os.path.join(data_folder, filename)
-        dst = os.path.join(test_folder, filename)
-        shutil.copy(src, dst)
-
-        # 复制对应的掩码图像到测试掩码文件夹
-        mask_filename = filename.replace('.jpg', '_mask.jpg')
-        mask_src = os.path.join(data_mask_folder, mask_filename)
-        mask_dst = os.path.join(test_mask_folder, mask_filename)
-        shutil.copy(mask_src, mask_dst)
 
 if __name__ == '__main__':
-    # 設定裝置
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
 
-    # 載入訓練好的模型
     model = load_model(device=device)
-    os.system('rmdir /s /q output_combine output_images test test_mask')
-    
-    # 调用 split_image 函数来选择500张图片
-    split_image()
 
-    # 加载测试数据集    
-    test_data = CustomDataset('test', 'test_mask', transform=T.Compose([T.ToTensor(), T.Resize((512, 512))]))
+    # 清除之前的結果
+    for folder in ['output_combine', 'output_images', 'test', 'test_mask']:
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+
+    os.makedirs('output_images', exist_ok=True)
+    os.makedirs('output_combine', exist_ok=True)
+
+    # 加載測試數據
+    test_data = CustomDataset('train_data', 'train_data_mask', transform=T.Compose([T.ToTensor(), T.Resize((256, 256))]))
     test_loader = DataLoader(test_data, batch_size=4, shuffle=False)
 
-    # 进行模型评估并保存输出结果
+    # 模型評估
     evaluate_model(model, test_loader, output_dir='output_images', device=device)
 
-    # 合并图片
-    combine_images('test', 'test_mask', 'output_images', 'output_combine')
+    # 合併圖片
+    combine_images('train_data', 'train_data_mask', 'output_images', 'output_combine')
--- train.py
+++ train.py
@@ -1,102 +1,94 @@
 # Import all the packages
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as T
 import torch.optim as optim
 import os
 from tqdm import tqdm
 from unet_utils  import UNet, CustomDataset
+from torch.cuda.amp import autocast, GradScaler
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
 
-# load current model to train
-def load_model_and_optimizer(model, optimizer, checkpoint_path):
-    if os.path.exists(checkpoint_path):
-        checkpoint = torch.load(checkpoint_path)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        start_epoch = checkpoint['epoch'] 
-        return start_epoch
-    else:
-        print(f"No checkpoint found at {checkpoint_path}. Starting training from scratch.")
-        return 0  
+def train(model, num_epochs, train_loader, optimizer, start_epoch):
+    loss_arr = []
+    model.zero_grad()
+    scalar = GradScaler()
+    for epoch in range(start_epoch, num_epochs):
+        print(f"\nEpoch {epoch+1}/{num_epochs}")
+        
+        batch_iterator = tqdm(train_loader, desc=f"Training,epoch={epoch+1}", leave=True)
+        
+        total_loss = 0
+        for count, (x, y, filename) in enumerate(batch_iterator):
+            model.train()
+            x = x.to(device)
+            y = y.to(device)
+            with autocast():
+                out = model(x)
+                loss = torch.nn.MSELoss()(out, y)
+            scalar.scale(loss).backward()
+            scalar.unscale_(optimizer=optimizer)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            scalar.step(optimizer=optimizer)
+            scalar.update()
+            optimizer.zero_grad()
+            total_loss += loss.item()
+            
+            batch_iterator.set_postfix(loss=loss.item())
 
+        avg_training_loss = total_loss / len(train_loader)
+        print(f'Epoch [{epoch+1}] training Loss: {avg_training_loss:.4f}')
+        
+        val_loss = eval(model, val_loader, epoch)
+        # scheduler.step(val_loss)
+        loss_arr.append((epoch+1,  avg_training_loss, val_loss))
 
-# training model
-def train(model, num_epochs, train_loader, optimizer, scheduler, start_epoch):
-  epoch_iterator = tqdm(range(start_epoch, num_epochs), desc="Epochs")
-  train_losses = []
-  for epoch in epoch_iterator:
-    batch_iterator = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}")
-    for count, (x, y, filename) in enumerate(batch_iterator):
-      model.train()
-      x = x.to(device)
-      y = y.to(device)
-      out = model(x)
-      out = torch.sigmoid(out)
-      # loss = loss_function(out, y)
-      loss = combined_loss(out, y)
-      optimizer.zero_grad()
-      loss.backward()
-      optimizer.step()
+        path = f'model_1107_{epoch + 1}.pt'
+        if epoch % 2:
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'epoch': epoch
+            }, path)
+            print(f"Model saved to {path}")
 
-      train_losses.append(loss.item())
+        print(loss_arr)
+    
 
-    val_loss = eval(model, val_loader, epoch)
-    scheduler.step(val_loss)
-    path = f'unet_model_{epoch + 1}.pt'
-    torch.save(
-      {
-        'model_state_dict' : model.state_dict(),
-        'optimizer_state_dict' : optimizer.state_dict(),
-        'epoch' : epoch
-      }
-    , path)
-    print(f"Model saved to {path}")
+    loss_array = np.array(loss_arr)
+    np.savetxt('loss.txt', loss_array, header='epoch, avg_training_loss, avg_val_loss', delimiter=', ', fmt='%s')
 
-# evaluate model with testing data after each epoch
 
 def eval(model, val_loader, epoch):
-  model.eval()
-  num_correct = 0
-  num_pixels = 0
-  total_loss = 0
-  with torch.no_grad():
-    val_iterator = tqdm(val_loader, desc=f"Evaluating Epoch {epoch + 1}")
-    for x, y, filename in val_iterator:
-      x = x.to(device)
-      y = y.to(device)
-      out_img = model(x)
-      probability = torch.sigmoid(out_img)
-      predictions = probability>0.5
-      num_correct += (predictions==y).sum()
-      num_pixels += torch.numel(predictions)
-      loss = combined_loss(probability, y)
-      total_loss += loss.item()
-  accuracy = num_correct / num_pixels
-  avg_loss = total_loss / len(val_loader)
-  print(f'Epoch [{epoch+1}] Validation Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}')
-  
-  return avg_loss  
-  
-
-def dice_loss(pred, target, smooth=1e-6):
-    pred = pred.contiguous()
-    target = target.contiguous()
+    model.eval()
+    num_correct = 0
+    num_pixels = 0
+    total_loss = 0
+    with torch.no_grad():
+        val_iterator = tqdm(val_loader, desc=f"Evaluating Epoch {epoch + 1}")
+        for x, y, filename in val_iterator:
+            x = x.to(device)
+            y = y.to(device)
+            out = model(x)
+            loss = torch.nn.MSELoss()(out, y)
+            probability = torch.sigmoid(out)
+            predictions = (probability > 0.5).float()
+            num_correct += (predictions == y).sum().item()  
+            num_pixels += torch.numel(predictions)
+            total_loss += loss.item()
     
-    intersection = (pred * target).sum(dim=2).sum(dim=2)
-    dice = (2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)
+    accuracy = num_correct / num_pixels  
+    avg_loss = total_loss / len(val_loader)
     
-    return 1 - dice.mean()
-
-def combined_loss(pred, target):
-    bce_loss = nn.BCEWithLogitsLoss()(pred, target)
+    print(f'Epoch [{epoch + 1}] Validation Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}')
     
-    dice = dice_loss(torch.sigmoid(pred), target)
-    
-    return bce_loss + dice
-
+    return avg_loss 
 
 if __name__ == '__main__':
     # Check the device we are using is GPU or CPU
@@ -105,37 +97,39 @@ if __name__ == '__main__':
     else:
         device = torch.device('cpu')
     print(device)
-
-    # # Create an UNet model object
+    
     model = UNet().cuda()
-    model_path = 'unet_model_15.pt'
-    model.load_state_dict(torch.load(model_path))
+    # Choosing Adam as our optimizer
+    optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4)
+
+    # model_path = 'model_1104_30.pt'
+    # checkpoint = torch.load(model_path)
+
+    # model.load_state_dict(checkpoint['model_state_dict'])
+
+    # # 加载优化器状态
+    # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    # for param_group in optimizer.param_groups:
+    #     param_group['lr'] = 1e-4
     # Constants for UNet model training process
     BATCH_SIZE = 4
-    NUM_EPOCHS = 15
-    IMG_WIDTH = 560
+    NUM_EPOCHS = 40
+    START_EPOCH = 0
+    IMG_WIDTH = 512
     IMG_HEIGHT = 512
     # Load data
-    all_data = CustomDataset('data', 'data_mask', T.Compose([T.ToTensor(), T.Resize((IMG_HEIGHT, IMG_WIDTH))]))
+    transform = T.Compose([T.ToTensor(), T.RandomHorizontalFlip(), T.RandomVerticalFlip(), T.Resize((IMG_HEIGHT, IMG_WIDTH), antialias=True)])
+    train_dataset = CustomDataset('data', 'data_mask', transform=transform)
+    val_dataset = CustomDataset('test', 'test_mask', transform=transform)
 
-    # Split data into train and val
-    train_indices, test_indices = train_test_split(range(len(all_data)), test_size=0.2, random_state=42)
+    # 創建 DataLoader
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
-    train_data = Subset(all_data, train_indices)
-    val_data = Subset(all_data, test_indices)
 
-    # Create loader for mini-batch gradient descent
-    train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=False)
-
-    # Choosing Adam as our optimizer with starting learning rate 10^-4
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
-
-    # define scheduler
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3)
+    # 定義 scheduler
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=3)
     
-    
-
-    train(model, NUM_EPOCHS, train_loader, optimizer, scheduler, start_epoch = 10)
-    # train(model, NUM_EPOCHS, train_loader, optimizer)
+  
+    train(model, NUM_EPOCHS, train_loader, optimizer, start_epoch = START_EPOCH)
     
